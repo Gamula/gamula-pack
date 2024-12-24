5446e5929402c46faf2213e76e6a4cb59f40a783
Saif Suleman <saif@visionituk.com>
2024-12-24 11:54:16 +0000

    dev

---
+++ run.sh
@@ -0,0 +1,12 @@
+curl -X POST -H "Content-Type: application/json" -d '{
+    "username": "Drone",
+    "avatar_url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSodFcGcfo72oDYs4w0NRBccDQ5L08m8VDnGg&s",
+    "embeds": [
+        {
+            "title": "gamula-pack [dev]",
+            "description": "Build #72 - Hash: test",
+            "color": 16711680
+        }
+    ]
+}' https://discord.com/api/webhooks/1321079701896302634/Ls-9ED65PnINeYehYfJgDf823dL3ksaAb3lUvc2lZlL7K7xl7cYKy_o856NcGcpx8f9p
+
